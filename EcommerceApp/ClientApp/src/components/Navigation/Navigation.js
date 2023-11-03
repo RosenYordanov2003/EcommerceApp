1cc38b5ac033104a4f5dabb0872c29fec43f8cf8
Rosen Yordanov <roskata182003@gmail.com>
2023-11-03 13:09:43 +0200

    Fix bug when clicking both men and women arrow categories

--- EcommerceApp/ClientApp/src/components/Navigation/Navigation.js
+++ EcommerceApp/ClientApp/src/components/Navigation/Navigation.js
@@ -1,50 +1,76 @@
 ﻿import { Link } from "react-router-dom";
 import Style from "../Navigation/Style.css"
-
+import { useState } from "react";
 
 export default function Navigation({ loadCategories, setIsActive }) {
+    const baseUrl = "https://localhost:7122/api/categories";
 
-    const baseUrl = "https://localhost:7122/api/categories"
-    function loadWomenCategories(event) {
-        event.target.classList.toggle("active-arrow");
-        if (event.target.classList.contains("active-arrow")) {
-            const genderQyeryParam = event.target.classList.contains('women-arrow') ? 'w' : 'm';
+    const [isMenArrowActive, setIsMenArrowActive] = useState(false);
+    const [isWomenArrowActive, setIsWomenArrowActive] = useState(false);
 
-            fetch(`${baseUrl}?gender=${genderQyeryParam}`)
-                .then(res => res.json())
-                .then(res => {
-                    loadCategories(res);
-                    setIsActive(true);
-                })
-            .catch((error) => console.error(error));
-        }
-        else {
+    function toggleMenCategories() {
+        setIsMenArrowActive(!isMenArrowActive);
+        if (isMenArrowActive) {
             setIsActive(false);
+        } else {
+            setIsActive(true);
+            loadCategoriesByGender("m");
         }
+      
+        setIsWomenArrowActive(false);
     }
+
+    function toggleWomenCategories() {
+        setIsWomenArrowActive(!isWomenArrowActive);
+        if (isWomenArrowActive) {
+            setIsActive(false);
+        } else {
+            setIsActive(true);
+            loadCategoriesByGender("w");
+        }
+     
+        setIsMenArrowActive(false);
+    }
+
+    function loadCategoriesByGender(gender) {
+        fetch(`${baseUrl}?gender=${gender}`)
+            .then((res) => res.json())
+            .then((res) => {
+                loadCategories(res);
+            })
+            .catch((error) => console.error(error));
+    }
+
     return (
         <nav>
-            <h2 className = "nav-logo">Fashion Store</h2>
+            <h2 className="nav-logo">Fashion Store</h2>
             <ul>
                 <li>
-                    <Link to="/Home">Men<i onClick={loadWomenCategories} className="men-arrow down-arrow fa-solid fa-angle-down"></i></Link>
+                    <Link to="/Home">
+                        Men{" "}
+                        <i
+                            onClick={toggleMenCategories}
+                            className={`men-arrow down-arrow fa-solid fa-angle-down ${isMenArrowActive ? "active-arrow" : "not-active-arrow"
+                                }`}
+                        ></i>
+                    </Link>
                 </li>
                 <li>
-                    <Link to="Home">Women <i onClick={loadWomenCategories} className="women-arrow down-arrow fa-solid fa-angle-down"></i></Link>
+                    <Link to="Home">
+                        Women{" "}
+                        <i
+                            onClick={toggleWomenCategories}
+                            className={`women-arrow down-arrow fa-solid fa-angle-down ${isWomenArrowActive ? "active-arrow" : ""
+                                }`}
+                        ></i>
+                    </Link>
                 </li>
-                {/*<li>*/}
-                {/*    <Link to="/Register">Sing Up</Link>*/}
-                {/*</li>*/}
-                {/*<li>*/}
-                {/*    <Link to="/Login">Login</Link>*/}
-                {/*</li>*/}
-                {/*<li>*/}
-                {/*    <Link to="/Logout">Logout</Link>*/}
-                {/*</li>*/}
                 <li>
-                    <Link to="/Cart"><i className="fa-solid fa-cart-shopping"></i></Link>
+                    <Link to="/Cart">
+                        <i className="fa-solid fa-cart-shopping"></i>
+                    </Link>
                 </li>
             </ul>
         </nav>
-    )
+    );
 }

ba96a289b305361ec554fbce0e7a6817028f791d
Rosen Yordanov <roskata182003@gmail.com>
2024-01-02 16:00:45 +0200

    Add initial pagination logic on All Product Reviews page

--- EcommerceApp/ClientApp/src/components/AllReviews/AllReviews.js
+++ EcommerceApp/ClientApp/src/components/AllReviews/AllReviews.js
@@ -3,21 +3,37 @@ import { loadAllReviewsAboutProduct } from "../../services/reviewService";
 import ReviewCard from "../../components/ReviewCard/ReviewCard";
 import AllReviewsStyle from "../AllReviews/AllReviewsStyle.css";
 import Notification from "../Notification/Notification";
+import Pager from "../Pager/Pager";
 
 export default function AllReviews() {
+
+    const [reviewsPerPage, setReviewsPerPage] = useState(5);
+    const [pageNumber, setPageNumber] = useState(1);
+    const [currentPageReviews, setCurrentPageReviews] = useState([]);
     const [reviews, setReviews] = useState([]);
     const [notification, setNotification] = useState(undefined);
 
     const pathArray = window.location.pathname.split('/');
-
     const productId = pathArray[2];
     const productCategory = pathArray[3];
+
     useEffect(() => {
         loadAllReviewsAboutProduct(productId, productCategory)
-            .then(res => setReviews(res))
+            .then(res => {
+                setReviews(res);
+                const startIndex = (pageNumber - 1) * reviewsPerPage;
+                const endIndex = pageNumber * reviewsPerPage;
+                setCurrentPageReviews(res.slice(startIndex, endIndex));
+            })
             .catch((error) => console.error(error));
 
     }, [])
+    useEffect(() => {
+        const startIndex = (pageNumber - 1) * reviewsPerPage;
+        const endIndex = pageNumber * reviewsPerPage;
+        setCurrentPageReviews(reviews.slice(startIndex, endIndex));
+
+    }, [pageNumber, reviewsPerPage])
 
     function removeReviewById(reviewId) {
         const filteredReviews = reviews.filter((review) => review.id !== reviewId);
@@ -28,14 +44,35 @@ export default function AllReviews() {
         setNotification(undefined);
     }
 
-    const reviewsResult = reviews?.map((review) => <ReviewCard review={review} key={review.id} removeReviewById={removeReviewById } />)
+    function pageNumberChange(newPageNumber) {
+        setPageNumber(newPageNumber);
+    }
+    function handlePagneItemsNumberChange (event) {
+        const value = event.target.options[event.target.selectedIndex].value;
+        setReviewsPerPage(value);
+        setPageNumber(1);
+    }
+
+    const reviewsResult = currentPageReviews?.map((review) => <ReviewCard review={review} key={review.id} removeReviewById={removeReviewById} />)
 
     const result = reviews?.length > 0 ? reviewsResult : <p className="no-reviews">This product has no reviews yet</p>
 
     return (
-        <div className="all-review-container">
-            {notification}
-            {result}
-        </div>
+        <>
+            <section className="comments-per-page">
+                <h4>Choose review number per page</h4>
+                <select value={reviewsPerPage} onChange={handlePagneItemsNumberChange}>
+                    <option value={8}>8</option>
+                    <option value={10}>10</option>
+                    <option value={20}>20</option>
+                    <option value={5}>Default</option>
+                </select>
+            </section>
+            <div className="all-review-container">
+                {notification}
+                {result}
+            </div>
+            <Pager pageNumberChange={pageNumberChange} totalReviewsLength={reviews.length} currentPage={pageNumber} endPage={Math.ceil(reviews.length / reviewsPerPage)} />
+        </>
     )
 }
---
+++ EcommerceApp/ClientApp/src/components/Pager/Pager.js
@@ -0,0 +1,24 @@
+﻿import PagerStyle from "../Pager/PagerStyle.css";
+
+export default function Pager({ totalReviewsLength, currentPage, endPage, pageNumberChange }) {
+
+    let result = []
+    for (let i = 1; i <=endPage ; i++) {
+        result.push(i);
+    }
+    function handlePageNumberClick(pageNumber) {
+        pageNumberChange(pageNumber);
+    }
+
+    return (
+        <div className="pager-container">
+            {currentPage - 1 > 0 && <button onClick={() => handlePageNumberClick(currentPage - 1)} className="previous-button">Previous</button>}
+
+            <ul className="pager-ul">
+                {result.map((x, index) => <li onClick={() => handlePageNumberClick(x)} key={index}>{x}</li>) }
+            </ul>
+
+            {currentPage + 1 <= endPage && <button onClick={() => handlePageNumberClick(currentPage + 1)} className="next-button">Next</button> }
+        </div>
+    )
+}

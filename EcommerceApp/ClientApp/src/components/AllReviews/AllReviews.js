781b5ed8efca243c4ab981a19e224308721a129b
Rosen Yordanov <roskata182003@gmail.com>
2024-01-03 12:13:25 +0200

    Add loading bar design on switching reviews number per page

--- EcommerceApp/ClientApp/src/components/AllReviews/AllReviews.js
+++ EcommerceApp/ClientApp/src/components/AllReviews/AllReviews.js
@@ -4,6 +4,7 @@ import ReviewCard from "../../components/ReviewCard/ReviewCard";
 import AllReviewsStyle from "../AllReviews/AllReviewsStyle.css";
 import Notification from "../Notification/Notification";
 import Pager from "../Pager/Pager";
+import { Grid } from 'react-loader-spinner';
 
 export default function AllReviews() {
 
@@ -12,6 +13,7 @@ export default function AllReviews() {
     const [currentPageReviews, setCurrentPageReviews] = useState([]);
     const [reviews, setReviews] = useState([]);
     const [notification, setNotification] = useState(undefined);
+    const [isLoading, setIsLoading] = useState(false);
 
     const pathArray = window.location.pathname.split('/');
     const productId = pathArray[2];
@@ -26,11 +28,12 @@ export default function AllReviews() {
                 setCurrentPageReviews(res.slice(startIndex, endIndex));
             })
             .catch((error) => console.error(error));
-
     }, [])
+
     useEffect(() => {
         const startIndex = (pageNumber - 1) * reviewsPerPage;
         const endIndex = pageNumber * reviewsPerPage;
+        configureLoading();
         setCurrentPageReviews(reviews.slice(startIndex, endIndex));
 
     }, [pageNumber, reviewsPerPage])
@@ -47,32 +50,54 @@ export default function AllReviews() {
     function pageNumberChange(newPageNumber) {
         setPageNumber(newPageNumber);
     }
-    function handlePagneItemsNumberChange (event) {
+    function handlePagneItemsNumberChange(event) {
         const value = event.target.options[event.target.selectedIndex].value;
         setReviewsPerPage(value);
         setPageNumber(1);
+    }
+    function configureLoading() {
+        setIsLoading(true);
+
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 500)
     }
 
     const reviewsResult = currentPageReviews?.map((review) => <ReviewCard review={review} key={review.id} removeReviewById={removeReviewById} />)
 
     const result = reviews?.length > 0 ? reviewsResult : <p className="no-reviews">This product has no reviews yet</p>
 
+
     return (
         <>
-            <section className="comments-per-page">
-                <h4>Choose review number per page</h4>
-                <select value={reviewsPerPage} onChange={handlePagneItemsNumberChange}>
-                    <option value={8}>8</option>
-                    <option value={10}>10</option>
-                    <option value={20}>20</option>
-                    <option value={5}>Default</option>
-                </select>
-            </section>
-            <div className="all-review-container">
-                {notification}
-                {result}
-            </div>
-            <Pager pageNumberChange={pageNumberChange} totalReviewsLength={reviews.length} currentPage={pageNumber} endPage={Math.ceil(reviews.length / reviewsPerPage)} />
+            {isLoading == true ? <Grid
+                height="80"
+                width="80"
+                color="#035096"
+                ariaLabel="grid-loading"
+                radius="12.5"
+                wrapperStyle={{}}
+                wrapperClass="spinner"
+                className="spinner-loading"
+                visible={true} /> :
+
+                <>
+                    {reviews.length > 0 && <section className="comments-per-page">
+                        <h4>Choose reviews number per page</h4>
+                        <select value={reviewsPerPage} onChange={handlePagneItemsNumberChange}>
+                            <option value={8}>8</option>
+                            <option value={10}>10</option>
+                            <option value={20}>20</option>
+                            <option value={5}>Default</option>
+                        </select>
+                    </section>}
+                    <div className="all-review-container">
+                        {notification}
+                        {result}
+                    </div>
+                    <Pager pageNumberChange={pageNumberChange} totalReviewsLength={reviews.length} currentPage={pageNumber} endPage={Math.ceil(reviews.length / reviewsPerPage)} />
+                </>
+            }
         </>
     )
 }

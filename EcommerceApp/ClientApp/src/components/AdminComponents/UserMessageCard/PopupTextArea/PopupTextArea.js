db3df1ed53906be7b63550d1d484fbabb446c136
Rosen Yordanov <roskata182003@gmail.com>
2024-02-27 13:56:17 +0200

    Add very well designed popup textarea container where admin can respond to user messages.

---
+++ EcommerceApp/ClientApp/src/components/AdminComponents/UserMessageCard/PopupTextArea/PopupTextArea.js
@@ -0,0 +1,27 @@
+﻿import Style from "../PopupTextArea/Style.css";
+import { useEffect, useState } from "react";
+
+export default function PopupTextArea({ handleOnCloseFunction, handleOnSendFunction, username }) {
+
+    const [message, setMessage] = useState('');
+    const [isActive, setIsActive] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsActive(true);
+        }, 50)
+    }, [])
+    function handleOnCloseNotification() {
+        setIsActive(false);
+        handleOnCloseFunction()
+    }
+
+    return (
+        <section className={`popup-text-area-container ${isActive === true ? 'active-text-area' : ''}`}>
+            <h2 className="username-response-title">Reply to: {username}</h2>
+            <i onClick={handleOnCloseNotification} className="fa-solid fa-xmark close-text-area"></i>
+            <textarea onChange={(e) => setMessage(e.target.value)}></textarea>
+            <button className="send-message-to-user" onClick={() => handleOnSendFunction(message)}>Send</button>
+        </section>
+    )
+}
--- EcommerceApp/ClientApp/src/components/AdminComponents/UserMessageCard/UserMessageCard.js
+++ EcommerceApp/ClientApp/src/components/AdminComponents/UserMessageCard/UserMessageCard.js
@@ -1,25 +1,42 @@
-﻿import Style from "../UserMessageCard/Style.css";
+﻿import { useState } from "react";
+import Style from "../UserMessageCard/Style.css";
+import PopupTextArea from "../UserMessageCard/PopupTextArea/PopupTextArea";
 
 export default function UserMessageCard({ userMessage }) {
 
+    const [popupTextArea, setPopupTextArea] = useState(undefined);
+
+    function closePopupTextArea() {
+        setTimeout(() => {
+            setPopupTextArea(undefined);
+        }, 470)
+    }
+    function sendMessage(message) {
+        console.log(message);
+    }
+
 
     return (
-        <article className="user-message-card">
-            <div className="user-message-header">
-                <h3>{userMessage.username}</h3>
-                <p>3h ago</p>
-            </div>
-            <p className="user-message-content">{userMessage.message}</p>
-            <section className="actions-container">
-                <div className="action">
-                    <i class="fa-solid fa-reply"></i>
-                    <button className="action-button">Reply</button>
+        <>
+            {popupTextArea !== undefined ? popupTextArea : ''}
+            <article className="user-message-card">
+                <div className="user-message-header">
+                    <h3>{userMessage.username}</h3>
+                    <p>3h ago</p>
                 </div>
-                <div className="action">
-                    <i class="fa-solid fa-trash"></i>
-                    <button className="action-button">Delete</button>
-                </div>
-            </section>
-        </article>
+                <p className="user-message-content">{userMessage.message}</p>
+                <section className="actions-container">
+                    <div onClick={() => setPopupTextArea(<PopupTextArea handleOnCloseFunction={closePopupTextArea} handleOnSendFunction={sendMessage} username={userMessage.username} />)} className="action">
+                        <i className="fa-solid fa-reply"></i>
+                        <button className="action-button">Reply</button>
+                    </div>
+                    <div className="action">
+                        <i className="fa-solid fa-trash"></i>
+                        <button className="action-button">Delete</button>
+                    </div>
+                </section>
+            </article>
+        </>
+
     )
 }

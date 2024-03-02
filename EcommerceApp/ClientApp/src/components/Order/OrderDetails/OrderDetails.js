d466569319cd8f79f692db6f249a1bd27139ed37
Rosen Yordanov <roskata182003@gmail.com>
2024-03-02 11:25:27 +0200

    Add OrderProductCard component with design

--- EcommerceApp/ClientApp/src/components/Order/OrderDetails/OrderDetails.js
+++ EcommerceApp/ClientApp/src/components/Order/OrderDetails/OrderDetails.js
@@ -1,19 +1,35 @@
 ﻿import { useState, useEffect } from "react";
 import { getOrderById } from "../../../services/orderService";
+import OrderProductCard from "../OrderProductCard/OrderProductCard";
 
 import Style from "../OrderDetails/Style.css";
 
 export default function () {
 
     const id = window.location.pathname.split('/')[2];
+    const [orderObject, setOrderObject] = useState(undefined);
 
 
     useEffect(() => {
         getOrderById(id)
-        .then(res => console.log(res))
+        .then(res => setOrderObject(res))
     }, [])
 
+    console.log(orderObject);
+
+    const orderProducts = orderObject?.products.map((product, index) => <OrderProductCard product={product} key={index} />);
+
     return (
-        <p>Details</p>
+        <div>
+            <h1 className="order-detials-title">Your Order with id: 03945885</h1>
+            <section className="order-details-section">
+                <h4 className="recap-title">Recap</h4>
+                <hr></hr>
+                <section className="order-recap">
+                    {orderProducts}
+                </section>
+            </section>
+        </div>
+       
     )
 }
---
+++ EcommerceApp/ClientApp/src/components/Order/OrderProductCard/OrderProductCard.js
@@ -0,0 +1,39 @@
+﻿import Style from "../OrderProductCard/Style.css";
+
+export default function OrderProductCard({ product }) {
+    console.log(product);
+    return (
+        <article className="order-product-recap">
+            <div className="recap-product-img-container">
+                <img src={product.imgUrl}></img>
+            </div>
+            <section className="product-order-section-details">
+                <div className="about-order-details-product">
+                    <p>Name:</p>
+                    <p className="order-product-prop">{product.name}</p>
+                </div>
+                <div className="about-order-details-product">
+                    <p>Category:</p>
+                    <p className="order-product-prop">{product.categoryName}</p>
+                </div>
+                <div className="about-order-details-product">
+                    <p>Size:</p>
+                    <p className="order-product-prop">{product.size}</p>
+                </div>
+                <div className="about-order-details-product">
+                    <p>Quantity:</p>
+                    <p className="order-product-prop">{product.quantity}</p>
+                </div>
+                <div className="about-order-details-product">
+                    <p>Price:</p>
+                    <p className="order-product-prop">${Number(product.price).toFixed(2)}</p>
+                </div>
+                <div className="about-order-details-product">
+                    <p>Total Price:</p>
+                    <p className="order-product-prop">${Number(product.price * product.quantity).toFixed(2)}</p>
+                </div>
+            </section>
+           
+        </article>
+    )
+}
